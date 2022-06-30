852293bcae6832ba29362351de870e9efd09e1e3
LilianD5 <97739931+LilianD5@users.noreply.github.com>
2022-06-30 14:19:48 +0200

    Header (Responsive Complet + Menu Burger)

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,1 +1,21 @@
 // Main.js
+
+let btnContainer = document.getElementById('burger-container')
+
+let topLine = document.getElementById('menu-button-top')
+let middleLine = document.getElementById('menu-button-center')
+let bottomLine = document.getElementById('menu-button-bottom')
+
+let nav = document.getElementById('header-nav-burger')
+
+console.log(btnContainer);
+console.log(topLine);
+console.log(middleLine);
+console.log(bottomLine);
+
+btnContainer.addEventListener('click', () => {
+    topLine.classList.toggle('top-transformation')
+    bottomLine.classList.toggle('bottom-transformation')
+    middleLine.classList.toggle('none')
+    nav.classList.toggle('new-position')
+})
