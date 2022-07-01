fb7ec52ac37b1b25c202be1b332ce84d6e8e14ad
LilianD5 <97739931+LilianD5@users.noreply.github.com>
2022-07-01 10:16:13 +0200

    Correction Menu Burger + Footer Full responsive

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,21 +1,23 @@
 // Main.js
 
-let btnContainer = document.getElementById('burger-container')
+function menuBurger(){
+    let btnContainer = document.getElementById('burger-container')
 
-let topLine = document.getElementById('menu-button-top')
-let middleLine = document.getElementById('menu-button-center')
-let bottomLine = document.getElementById('menu-button-bottom')
+    let topLine = document.getElementById('menu-button-top')
+    let middleLine = document.getElementById('menu-button-center')
+    let bottomLine = document.getElementById('menu-button-bottom')
+    
+    let nav = document.getElementById('header-nav-burger')
+    
+    
+    btnContainer.addEventListener('click', () => {
+        topLine.classList.toggle('top-transformation')
+        bottomLine.classList.toggle('bottom-transformation')
+        middleLine.classList.toggle('none')
+        nav.classList.toggle('new-position');
+    })
+}
 
-let nav = document.getElementById('header-nav-burger')
+menuBurger();
 
-console.log(btnContainer);
-console.log(topLine);
-console.log(middleLine);
-console.log(bottomLine);
 
-btnContainer.addEventListener('click', () => {
-    topLine.classList.toggle('top-transformation')
-    bottomLine.classList.toggle('bottom-transformation')
-    middleLine.classList.toggle('none')
-    nav.classList.toggle('new-position')
-})
